6d726d4cb5fb3a04c6a1ac9117377cb12dcf9834
leuhsouza <leuhsouza@gmail.com>
2024-03-13 05:50:59 -0300

    wip: add models and

--- Associations/db/migrate/20240313082744_create_books.rb
+++ Associations/db/migrate/20240313082744_create_books.rb
@@ -2,6 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.1]
   def change
     create_table :books do |t|
       t.string :name
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
